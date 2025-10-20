6a3606fde3ba7f35c33370355ed90062f03f2bd6
mathys <mathys.j45@gmail.com>
2025-10-20 12:22:37 +0000

    Exercice 3

---
+++ Exercice_3/functions.h
@@ -0,0 +1,13 @@
+#ifndef __FUNCTIONS_H__
+#define __FUNCTIONS_H__
+struct s_character {
+    char *name;
+    int strength;
+    int intelligence;
+    int wisdom;
+    int agility;
+    int endurance;
+};
+
+typedef struct s_character character;
+#endif
