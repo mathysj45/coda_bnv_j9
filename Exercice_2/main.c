e0d61580994fe3663120a98508463cdc061ed00a
mathys <mathys.j45@gmail.com>
2025-10-20 12:08:40 +0000

    Exercice 2

---
+++ Exercice_2/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "functions.h"
+
+void fill_struct(character * perso)
+{
+    perso->name = "Michel";
+    perso->strength = 100;
+    perso->intelligence = 200;
+    perso->wisdom = 0;
+    perso->agility = 150;
+    perso->endurance = 75;
+}
+
+int main()
+{
+    character perso;
+    fill_struct(&perso);
+    printf("Le héro de notre histoire s'appelle %s. Sa force est de %d, son intelligence de %d, sa sagesse de %d, sont agilité de %d et son endurance de %d.\n", perso.name, perso.strength, perso.intelligence, perso.wisdom, perso.agility, perso.endurance);
+    exit(0);
+}
